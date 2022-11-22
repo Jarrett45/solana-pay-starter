ee8ef7479c9f68a534367de847f5b2ade1e2ccf2
Jarrett <rinbatian9999@gmail.com>
2022-11-22 19:32:41 +0800

    Add the connect wallet button

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import HeadComponent from '../components/Head';
+import { PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
+import dynamic from "next/dynamic";
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "Jarrett";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  //Dynamic import `WalletMultiButton` to prevent hydration error
+  const WalletMultiButtonDynamic = dynamic(
+    async () => 
+     ( await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false}
+  );
+
+  //This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://i.giphy.com/media/39tzv6VVmqdXstQumX/200w.webp" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButtonDynamic className="cta-button conncet-wallet-button" />
+      </div>
+    </div>
+  );
   
   
   return (
@@ -13,12 +36,14 @@ const App = () => {
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> 😳 NFT Store 😈</p>
+          <p className="sub-text">The only NFT store that accepts sh*tcoins</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? "Connected!" : renderNotConnectedContainer()}
+          
         </main>
 
         <div className="footer-container">
